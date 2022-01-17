60ba7e5f489099e1a0183d53e657830398b1ec66
jose.alejo@millicom.com <jose.alejo@millicom.com>
2022-01-17 17:25:02 -0600

    Se agrega RDS postgres

--- config/database.js
+++ config/database.js
@@ -2,10 +2,16 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'merchant-platform-landing-cms.cpdprsgplcjh.us-east-1.rds.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'merchant-platform-landing-cms'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'landingcms'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: false,
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
